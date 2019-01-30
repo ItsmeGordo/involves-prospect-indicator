d57a0f26063e6670a8c1b6fdc52b8361af3b5daa
Renato Müller Rosa <renatogordooo@gmail.com>
2019-01-30 22:35:19 +0000

    Primeiro commit

---
+++ src/main/java/br/com/involves/prospectIndicator/ProspectIndicatorApplication.java
@@ -0,0 +1,14 @@
+package br.com.involves.prospectIndicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectIndicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectIndicatorApplication.class, args);
+	}
+
+}
+
